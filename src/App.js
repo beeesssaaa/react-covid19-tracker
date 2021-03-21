dffb2a32831e2b342750efac8425e5eb6652d3bc
beeesssaaa <13708919+beeesssaaa@users.noreply.github.com>
2021-03-21 15:05:40 +0900

    Added cards, table, graphs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,102 @@
+import { Card, CardContent, FormControl, MenuItem, Select } from '@material-ui/core';
+import { useEffect, useState } from 'react';
 import './App.css';
+import InfoBox from './InfoBox';
+import LineGraph from './LineGraph';
+import Map from './Map';
+import Table from './Table';
+import { sortData } from './util';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState('worldwide')
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data)
+    });
+  }, []);
+
+  //  https://disease.sh/v3/covid-19/countries
+
+  useEffect(() => {
+    // async -> send a request, wait for it do something with info
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ( {
+              name: country.country,    // United States, United Kingdom
+              value: country.countryInfo.iso2,    // USA, UK
+            }));
+
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
+          setCountries(countries);
+        })
+    };
+    getCountriesData();
+  }, [])
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+
+    const url = countryCode === 'worldwide' ? 'https://disease.sh/v3/covid-19/all' : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+
+    await fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      setCountry(countryCode);
+      // ALl of the data
+      // from the country response
+      setCountryInfo(data);
+    });
+  };
+
+  console.log("COUNTRY INFO >>>", countryInfo);
+
   return (
     <div className="app">
-      <h1>Let's build a COVID-19 tracker</h1>
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+            <FormControl className="app__dropdown">
+              <Select variant="outlined" 
+                onChange={onCountryChange}
+                value={country} >
+                <MenuItem value="worldwide">WorldWide</MenuItem>
+                {
+                  /* Loop through all the conuntries and show a dropdown list of the options */
+                  countries.map(country => (
+                    <MenuItem value={country.value}>{country.name}</MenuItem>
+                  ))
+                }
+              </Select>
+            </FormControl>
+        </div>
+
+
+        <div className="app__stats">
+          <InfoBox title="Coroanvirus cases" cases={countryInfo.todayCases} total={countryInfo.cases} />
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered}  total={countryInfo.recovered} />
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+        </div>
+        {/* Map */}
+        <Map />
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Countries</h3>
+          <Table countries={tableData} />
+          <h3>World wide new cases</h3>
+          <LineGraph />
+        </CardContent>
+      </Card>
     </div>
   );
 }
---
+++ src/InfoBox.js
@@ -0,0 +1,21 @@
+import { Card, CardContent, Typography } from '@material-ui/core'
+import React from 'react'
+
+function InfoBox({title, cases, total}) {
+    return (
+        <Card className="infoBox">
+            <CardContent>
+                {/* Title */}
+                <Typography className="infoBox__title" color="textSecondary">{title}</Typography>
+                {/* +120k Number of Cases */}
+                <h2 className="infoBox__cases">{cases}</h2>
+                {/* 1.2M Total */}
+                <Typography className="infoBox__total" color="textSecondary">
+                    {total} Total
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
